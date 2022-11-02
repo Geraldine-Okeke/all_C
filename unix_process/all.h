3b84e61e3629dc6be48ab74e63e910a0e812e8f6
Delphybaby <me.ejiofor@unizik.edu.ng>
2022-11-02 11:12:47 +0100

    0

---
+++ unix_process/all.h
@@ -0,0 +1,11 @@
+#ifndef ALL_H
+#define ALL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+#endif
---
+++ unix_process/fork.c
@@ -0,0 +1,13 @@
+#include "all.h"
+/*
+ *main: entry point
+ *fork(): a function that creates two process , child and parent process
+ *
+ *return 0;
+ */
+int main()
+{
+	fork();
+		printf("hello world\n");
+	return 0;
+}
