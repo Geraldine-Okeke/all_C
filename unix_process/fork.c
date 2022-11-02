d1f18b2c76c78e2920c8b82387cb5bc79f9e30a5
Delphybaby <me.ejiofor@unizik.edu.ng>
2022-11-02 11:29:44 +0100

    0

--- unix_process/fork.c
+++ unix_process/fork.c
@@ -11,3 +11,9 @@ int main()
 		printf("hello world\n");
 	return 0;
 }
+
+/* calling the fork() twice will print the above four times.
+ * the arithmetics of this is 2^n , where any is the number of times fork()is called
+ * from the above, calling fork() 3 times, will print the input 8times , i.e 2^3.
+ */
+
